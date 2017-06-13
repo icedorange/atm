429807fb5ac7fdd3b21641fdf2f71ebdd7689dcd
东方皓 <1520525869@qq.com>
2017-06-13 16:13:30 +0800

    baseServlet

--- src/com/atm/servlet/LoginServlet.java
+++ src/com/atm/servlet/LoginServlet.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -69,14 +68,4 @@ public class LoginServlet extends BaseServlet {
 
 	}
 	
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
-
 }
