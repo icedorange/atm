64df89e97931f4eccc9b2cd273b1dc7cbbbf4798
东方皓 <1520525869@qq.com>
2017-06-16 12:01:02 +0800

    modify

---
+++ src/com/atm/filter/CharacterEncode.java
@@ -0,0 +1,55 @@
+package com.atm.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class CharacterEncoding
+ */
+@WebFilter("/*")
+public class CharacterEncode implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public CharacterEncode() {
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// place your code here
+		HttpServletRequest req = (HttpServletRequest) request;
+		req.setCharacterEncoding("UTF-8");
+
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.setCharacterEncoding("UTF-8");
+		//res.setContentType("text/html;charset=utf-8");
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
---
+++ src/com/atm/listener/MyListener.java
@@ -0,0 +1,103 @@
+package com.atm.listener;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class MyListener
+ *
+ */
+@WebListener("/MyListener")
+public class MyListener implements HttpSessionAttributeListener, HttpSessionListener {
+
+	/**
+	 * Default constructor.
+	 */
+	public MyListener() {
+	}
+
+	private static int count = 0;
+
+	private Set<Integer> set = new HashSet<>();
+
+	private Map<Integer, Object> userMap = new HashMap<>();
+
+	/**
+	 * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+	 */
+	public void sessionCreated(HttpSessionEvent se) {
+	}
+
+	/**
+	 * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+	 */
+	public void sessionDestroyed(HttpSessionEvent se) {
+	}
+
+	/**
+	 * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+	 */
+	public void attributeAdded(HttpSessionBindingEvent se) {
+		String name = se.getName();
+		if (name.equals("cardId")) {
+			int value = Integer.parseInt(String.valueOf(se.getValue()));
+			count++;
+			set.add(value);
+			// 获得cardId的session
+			Object obj = userMap.get(value);
+			if (obj == null) {
+				userMap.put(value, se.getSession());
+			} else {
+				// 将obj转为session类型
+				HttpSession session = (HttpSession) obj;
+				// 将此session中的属性删除
+				session.removeAttribute("cardId");
+				set.add(value);
+				userMap.put(value, se.getSession());
+				System.out.println(se.getValue() + "用户被迫下线");
+				session.setAttribute("msg", "您的账户在其它浏览器登录，此会话被下线");
+			}
+
+			System.out.println("人气（30分钟内活跃的用户）A：" + count);
+			System.out.println("在线人数A：" + set.size());
+			System.out.println("在线账户数A：" + userMap.size());
+		}
+	}
+
+	/**
+	 * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+	 */
+	public void attributeRemoved(HttpSessionBindingEvent se) {
+		String name = se.getName();
+		if (name == "cardId") {
+			int value = Integer.parseInt(String.valueOf(se.getValue()));
+			count--;
+			set.remove(value);
+			// 获得cardId的session
+			Object obj = userMap.get(value);
+			// 将obj转为session类型
+			HttpSession session = (HttpSession) obj;
+			// 将此session中的属性删除
+			session.removeAttribute("cardId");
+			System.out.println("人气（30分钟内活跃的用户）R：" + count);
+			System.out.println("在线账户数目R：" + set.size());
+			System.out.println("在线用户数R：" + userMap.size());
+		}
+	}
+
+	/**
+	 * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+	 */
+	public void attributeReplaced(HttpSessionBindingEvent se) {
+	}
+
+}
